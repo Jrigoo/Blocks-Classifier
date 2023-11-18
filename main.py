db6bbacf2f0080a878cce759ccb46b60f7a98d6b
Juan Riquelme <pk.juanr@gmail.com>
2023-11-17 20:32:39 -0500

    :sparkles: Project v1.0 Finished
    Robot classifier done

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 from remote.coppelia import Coppelia
-import numpy as np
-import time
 
 if __name__ == '__main__':
-    c = Coppelia()
-    qo = [65, -45, 45, 0, 0, 0]
-    T06 = c.foward_kinematics(qo)
-    q = c.inverse_kinematics(np.array(T06, dtype=np.float64).squeeze())
-    c.move_joints(q)
-    time.sleep(2)
+    coppelia = Coppelia()
+    block_counter = {
+        "Blue": 0,
+        "Red": 0,
+        "Green": 0,
+        "White": 0
+    }
+    block_height = 0.02
+
+    try:
+        while True:
+            coppelia.set_conveyor(0.05)
+            color = coppelia.classify_blocks()
+
+            if color:
+                print("The color detected is {}".format(color))
+
+                # Leemos el sensor de proximidad
+                obj_detection = coppelia.read_proximity_sensor()
+                while not obj_detection:
+                    obj_detection = coppelia.read_proximity_sensor()
+                coppelia.set_conveyor(0)
+
+                # Movemos el mirobot A regoger el bloque
+                x, y, z = [0.27515,  -0.00976,  0.08999]
+                coppelia.move_robot([x, y, z])
+
+                # Gripper ON
+                coppelia.set_gripper(1)
+                coppelia.move_robot([x, y, z+0.1])
+                coppelia.move_robot([0.19822, 0, 0.22858])
+
+                # Movemos el robot al cup 1
+                x, y, z = list(coppelia.cups_pos[color])
+                coppelia.move_robot([x, y, 0.220])
+                coppelia.move_robot(
+                    [x, y, z + block_counter[color]*block_height])
+                coppelia.set_gripper(0)
+                coppelia.move_robot([x, y, 0.220])
+                coppelia.move_robot([0.19822, 0, 0.22858])
+
+                block_counter[color] += 1
+
+    except KeyboardInterrupt:
+        print("closed.")
--- remote/coppelia.py
+++ remote/coppelia.py
@@ -1,15 +1,15 @@
-from IPython.display import display
 from remote import sim
 import numpy as np
 import sympy as sp
+import cv2
 import time
 
 
 class Coppelia:
     def __init__(self, port=19990):
-        self.client_id = self.connect(port)
+        self.client_id = self.__connect(port)
         self.__init_handles()
-        self.__init_blocks(quantity=1)
+        self.__init_cups()
 
         # Medidas del robot - Ajustadas según el Coppelia
         self.L1 = 29.55
@@ -19,7 +19,7 @@ class Coppelia:
         self.L5 = 168.67
         self.E = 58.85  # Sin gripper 24.28
 
-    def connect(self, port):
+    def __connect(self, port):
         """ 
         Connect to coppelia local server
         param: connectionport
@@ -34,67 +34,48 @@ class Coppelia:
             print("No se pudo conectar")
         return client_id
 
-    def __init_blocks(self, quantity=1):
+    def __init_cups(self):
         """ 
         Init simulation blocks positions and handles 
         """
-        # Pickup Parts
-        self.blocks = [sim.simxGetObjectHandle(
-            self.client_id, f"./pickupPart{i+1}/pickPos", sim.simx_opmode_blocking)[1] for i in range(quantity)]
+        cups_colors = ["Blue", "Red", "White", "Green"]
+        quantity = len(cups_colors)
+        self.cup_handlers = [sim.simxGetObjectHandle(
+            self.client_id, f"./Cup{cups_colors[i]}/pickPos", sim.simx_opmode_blocking)[1] for i in range(quantity)]
 
-        # Block Positions
-        self.blocks_pos = [np.around(np.array(sim.simxGetObjectPosition(
-            self.client_id, self.blocks[i], -1, sim.simx_opmode_blocking)[1]), 5) for i in range(quantity)]
+        self.cups_pos = {cups_colors[i]: np.around(np.array(sim.simxGetObjectPosition(
+            self.client_id, self.cup_handlers[i], -1, sim.simx_opmode_blocking)[1]), 5) for i in range(quantity)}
 
     def __init_handles(self):
         """
         Initializes handles for joints and other 
         parts of the robots
         """
-        # Gripper
+        # Gripper handler
         self.gripper = sim.simxGetObjectHandle(
             self.client_id, "./Mirobot/vacuumGripper", sim.simx_opmode_blocking)[1]
 
-        # Final Tip (efector Final)
+        # Final Tip (efector Final) hanler
         self.tip = sim.simxGetObjectHandle(
             self.client_id, "./Mirobot/tip", sim.simx_opmode_blocking)[1]
 
-        # Joints
+        # Joints handler
         self.joints = [sim.simxGetObjectHandle(
             self.client_id, f"./Mirobot/joint{i+1}", sim.simx_opmode_blocking)[1] for i in range(6)]
 
-        #
+        # Camera Handler
+        self.camera = sim.simxGetObjectHandle(
+            self.client_id, "./camera", sim.simx_opmode_oneshot_wait)[1]
 
-    def move_joints(self, q):
-        """ 
-         Move robot joins to specific angles. Prints
-         tip position and calc DH model
-         param: q -> Array of angles in degrees
-        """
-        # Calc model using DH
-        T06 = self.foward_kynematics(q)
-        print(f"Matriz T06 calculada con DH utilizando: {q}\n")
-        sp.pprint(T06)
-        time.sleep(0.5)
+        # Camera init
+        _, _, _ = sim.simxGetVisionSensorImage(
+            self.client_id, self.camera, 0, sim.simx_opmode_streaming)
 
-        # Executes for each join, each move
-        for qval, qi in zip(q, self.joints):
-            sim.simxSetJointTargetPosition(
-                self.client_id, qi, np.deg2rad(qval), sim.simx_opmode_oneshot)
+        # Proximity Sensor handler
+        self.proximity_sensor = sim.simxGetObjectHandle(
+            self.client_id, "./proximity_sensor", sim.simx_opmode_blocking)[1]
 
-        time.sleep(0.5)
-
-        # Calc final position
-        tip_position = np.around(np.array(sim.simxGetObjectPosition(
-            self.client_id, self.tip, -1, sim.simx_opmode_blocking)[1]), 5)
-
-        # Print final tip position
-        print(f"\nPosición del Efector según Coppelia: {tip_position*1000}\n")
-
-        # Return T06
-        return np.array(T06, dtype=np.float64).squeeze()
-
-    def Tdh(self, d, theta, a, alpha):
+    def __Tdh(self, d, theta, a, alpha):
         """ 
         Function that calculates the result TH matrix 
         for DH method
@@ -124,7 +105,7 @@ class Coppelia:
 
         # Transformaciones homogéneas
         T01, T12, T23, T34, T45, T56 = [
-            self.Tdh(ds[i], thetas[i], a_s[i], alphas[i]) for i in range(6)]
+            self.__Tdh(ds[i], thetas[i], a_s[i], alphas[i]) for i in range(6)]
 
         # Transformación homogénea final
         return sp.simplify(T01*T12*T23*T34*T45*T56).applyfunc(lambda x: '{:.4f}'.format(x))
@@ -164,7 +145,7 @@ class Coppelia:
         alphas = np.array([90, 0, 90])*(np.pi/180)
 
         # Obtengo las transformaciones hasta 03
-        T01, T12, T23 = [self.Tdh(ds[i], thetas[i], a_s[i], alphas[i])
+        T01, T12, T23 = [self.__Tdh(ds[i], thetas[i], a_s[i], alphas[i])
                          for i in range(3)]
         T03 = sp.simplify(T01*T12*T23).applyfunc(lambda x: '{:.4f}'.format(x))
         T03 = np.array(T03, dtype=np.float64).squeeze()
@@ -176,20 +157,90 @@ class Coppelia:
         R36 = np.dot(R30, R06)
 
         # Obtenemos los angulos de euler
-        # q5 = np.rad2deg(np.arctan2(R36[2, 2], np.sqrt(1 - R36[2, 2]**2)))
-        # q4 = np.rad2deg(np.arctan2(-R36[0, 2], -R36[1, 2]))
-        # q6 = np.rad2deg(np.arctan2(R36[2, 0], -R36[2, 1]))
-        # return np.around(np.array([q1, q2, q3, 90 - q4, q5, 90 - q6]), 2)
-
-        # Obtenemos los angulos de euler
         q5 = np.rad2deg(np.arcsin(R36[2, 2]))
         q4 = np.rad2deg(np.arctan2(-R36[1, 2], -R36[0, 2]))
         q6 = np.rad2deg(np.arctan2(-R36[2, 1], R36[2, 0]))
 
         return np.around(np.array([q1, q2, q3, q4, q5, q6]), 2)
 
-    def set_gripper(self, state=False):
+    def move_robot(self, position):
+        x, y, z = np.array(position)*1000
+        T06 = np.array(
+            [[1, 0, 0, x], [0, -1, 0, y], [0, 0, -1, z], [0, 0, 0, 1]])
+        joints = self.inverse_kinematics(T06)
+        self.move_joints(joints)
+        time.sleep(1)
+
+    def move_joints(self, q):
+        """ 
+         Move robot joins to specific angles. Prints
+         tip position and calc DH model
+         param: q -> Array of angles in degrees
+        """
+        # Executes for each join, each move
+        for qval, qi in zip(q, self.joints):
+            sim.simxSetJointTargetPosition(
+                self.client_id, qi, np.deg2rad(qval), sim.simx_opmode_oneshot)
+
+    def set_gripper(self, state):
         """ 
         Void Method to activate gripper 
         """
-        pass
+        sim.simxCallScriptFunction(self.client_id, './Mirobot', sim.sim_scripttype_childscript,
+                                   'setGripperOn', [state], [], [], bytearray(), sim.simx_opmode_blocking)
+        time.sleep(0.5)
+
+    def set_conveyor(self, velocity=0):
+        """ 
+        Void Method to activate conveyor
+        """
+        sim.simxCallScriptFunction(self.client_id, './Mirobot', sim.sim_scripttype_childscript,
+                                   'setConveyor', [], [velocity], [], bytearray(), sim.simx_opmode_blocking)
+
+    def read_proximity_sensor(self):
+        """ 
+        Read Proximity Sensor values
+        Return: Sensor state, obj_coordinates,obj_handle
+        """
+        state = sim.simxReadProximitySensor(
+            self.client_id, self.proximity_sensor, sim.simx_opmode_blocking)[1]
+        return state
+
+    def classify_blocks(self):
+        """ 
+        This method shows Coppelia Camera Vision Sensor
+        and Classifies White, Blue, Red, Green Colors
+        """
+        returnCode, resolution, image = sim.simxGetVisionSensorImage(
+            self.client_id, self.camera, 0, sim.simx_opmode_buffer)
+        color = ""
+        if returnCode == 0 and image:
+            img = np.array(image, dtype=np.uint8)
+            img.resize([resolution[0], resolution[1], 3])
+            img = cv2.resize(img, (512, 512))
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+            HSV = {
+                "Blue": ([110, 50, 50], [130, 255, 255]),
+                "Red": ([0, 100, 100], [10, 255, 255]),
+                "Green": ([25, 52, 72], [102, 255, 255]),
+                "White": ([0, 0, 255], [0, 0, 255])
+            }
+
+            images = ["Blue", "Red", "Green", "White"]
+            for img_name in images:
+                img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                lower = np.array(HSV[img_name][0], np.uint8)
+                upper = np.array(HSV[img_name][1], np.uint8)
+                mask = cv2.inRange(img_hsv, lower, upper)
+
+                kernel = np.ones((5, 5), "uint8")
+                mask = cv2.dilate(mask, kernel)
+                res = cv2.bitwise_and(img_hsv, img_hsv, mask=mask)
+                gray_image = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+                _, hierarchy = cv2.findContours(
+                    gray_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                if hierarchy is not None:
+                    color = img_name
+            return color
